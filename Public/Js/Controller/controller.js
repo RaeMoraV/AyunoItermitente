a33f824b82c50d78f68fd5d4ed4026e9c08a3d60
usoza0497 <asozas@ucenfotec.ac.cr>
2022-11-02 22:14:15 -0600

    Update Perfil
    Rollback del nav al antiguo diseno
    
    Y adicion de tablas para las secciones de registro

--- Public/Js/Controller/controller.js
+++ Public/Js/Controller/controller.js
@@ -1,3 +1,5 @@
+/* NAV Mobile Menu */
+
 document.getElementById('menuButton').addEventListener('click', mostrarMenu);
 
 document.getElementById('backMenu').addEventListener('click', ocultarMenu);
@@ -16,43 +18,17 @@ function ocultarMenu() {
 }
 
 
+/* Perfil - Seccion Informacion */
 
-//Ejecutar función en el evento click
-document.getElementById("btn_open").addEventListener("click", open_close_menu);
+imprimirPeso();
 
-//Declaramos variables
-var side_menu = document.getElementById("menu_side");
-var btn_open = document.getElementById("btn_open");
-var body = document.getElementById("body");
+let registrarPeso = document.querySelector('#buttonRegistro');
+let inputPeso = document.querySelector('#buttonRegistro');
+let fechaPeso = document.querySelector('#buttonRegistro');
+let ims = document.querySelector('#buttonRegistro');
 
-//Evento para mostrar y ocultar menú
-    function open_close_menu(){
-        body.classList.toggle("body_move");
-        side_menu.classList.toggle("menu__side_move");
-    }
+/* Perfil - Seccion Peso */
 
-//Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
 
-if (window.innerWidth < 760){
 
-    body.classList.add("body_move");
-    side_menu.classList.add("menu__side_move");
-}
-
-//Haciendo el menú responsive(adaptable)
-
-window.addEventListener("resize", function(){
-
-    if (window.innerWidth > 760){
-
-        body.classList.remove("body_move");
-        side_menu.classList.remove("menu__side_move");
-    }
-
-    if (window.innerWidth < 760){
-
-        body.classList.add("body_move");
-        side_menu.classList.add("menu__side_move");
-    }
-
-});
+/* Perfil - Seccion Enfermedades */
