e83d2158452ab5986b58a8d0fcec2995ff60c345
lariamz <116697103+lariamz@users.noreply.github.com>
2022-11-07 17:20:45 -0600

    Metas

--- Public/Js/Controller/perfil.js
+++ Public/Js/Controller/perfil.js
@@ -122,11 +122,11 @@ function blockDisplay(pOpcionSM){
             document.getElementById('recetas').style.display='block';
             break;
         default:
-            document.getElementById('informacion').style.display='block';
+            document.getElementById('informacion').style.display='none';
             document.getElementById('peso').style.display='none';
             document.getElementById('enfermedades').style.display='none';
             document.getElementById('actividadFisica').style.display='none';
-            document.getElementById('logroObjetivos').style.display='none';
+            document.getElementById('logroObjetivos').style.display='block';
             document.getElementById('planAyuno').style.display='none';
             document.getElementById('recetas').style.display='none';
             break;
@@ -417,6 +417,7 @@ function orderEnfermedadTable () {
     }
 }
 
+/* Perfil - Logros - Tabla  ---------------------------------------*/
 
 
 /* Perfil - Seccion Ayuno - Tabla  ---------------------------------------*/
