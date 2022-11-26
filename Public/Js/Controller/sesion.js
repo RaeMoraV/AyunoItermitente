77bf629289fb42ba70be1805b38b0794ef92fbf2
UncleRae <jomorav@ucenfotec.ac.cr>
2022-11-25 19:06:21 -0600

    services and controller services

--- Public/Js/Controller/sesion.js
+++ Public/Js/Controller/sesion.js
@@ -5,9 +5,9 @@ IdentificarUsuarioLogueado();
 function IdentificarUsuarioLogueado(){
     let result = GetSesionActiva();
     if (result != null) {
-        document.getElementById('adminTitulo').innerHTML = 'Bienvenid@ ' + result.Nombre + ' ' + result.Apellido1 +
-        '<br> su usuario es: '+ result.User +
-        '<br> su rol es: '+ result.Rol;
+        document.getElementById('adminTitulo').innerHTML = 'Bienvenid@ ' + result.Nombre + ' ' + result.Apellido +
+        '<br> Usuario: '+ result.User +
+        '<br> Apellido: '+ result.Rol;
     }
 }
 
