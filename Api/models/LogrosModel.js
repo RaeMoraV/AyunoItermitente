a05604ea1a696d640609a1870f71a96bf257d8dc
HAUNTEDsoulG <svillalobosc@ucenfotec.ac.cr>
2022-11-25 21:46:12 -0600

    Update LogrosModel.js
    modulo nuevo

--- Api/models/LogrosModel.js
+++ Api/models/LogrosModel.js
@@ -3,10 +3,11 @@
 const mongoose = require("mongoose");
 const schemaLogro = new mongoose.Schema({
     Logro: { type: Number, required: true, unique: false },
+    NombredeLogro: {type:String,required:true,unique:true},
     TipoLogro: {type:String,required:true,unique:true},
     CondicionLogro: {type:String,required:true,unique:true},
     Medalla: {type:String,required:true,unique:true},
-    Estado: {type:String,required:true,unique:true},
+    Estado: {type:Number,required:true,unique:false},
 
     
 });
