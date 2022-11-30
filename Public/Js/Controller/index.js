7e2ce3e8d36cf1badfd047b9391adb99bd711591
usoza0497 <asozas@ucenfotec.ac.cr>
2022-11-29 20:49:44 -0600

    Validaciones Aun sin completar del Registro

--- Public/Js/Controller/index.js
+++ Public/Js/Controller/index.js
@@ -74,8 +74,42 @@ function actualizarFecha(){
     inputFecha = `${numDia.value},${numMes.value},${numAnio.value}`;
 }
 
+
+
 submitRegistrate.addEventListener('click', actualizarFecha);
 
+submitRegistrate.addEventListener('click', function(inputNombre, inputApellido, inputEmail, inputEstatura, inputPesoDeseado, inputGenero, inputFecha, fotoPerfil){
+    if (inputNombre.value == '' || inputNombre.value == null || inputNombre.value == undefined) {
+        imprimirMsjError('El campo Nombre es requerido');
+    }
+    if (inputApellido.value == '' || inputApellido.value == null || inputApellido.value == undefined) {
+        imprimirMsjError('El campo Apellido es requerido');
+    }
+    if (inputEmail.value == '' || inputEmail.value == null || inputEmail.value == undefined) {
+        imprimirMsjError('El campo Correo es requerido');
+    }
+    if (inputEstatura.value == '' || inputEstatura.value == null || inputEstatura.value == undefined) {
+        imprimirMsjError('El campo Estatura es requerido');
+    }
+    if (inputPesoDeseado.value == '' || inputPesoDeseado.value == null || inputPesoDeseado.value == undefined) {
+        imprimirMsjError('El campo Peso es requerido');
+    }
+    if (inputGenero.value == '' || inputGenero.value == null || inputGenero.value == undefined) {
+        imprimirMsjError('El campo Genero es requerido');
+    }
+    if (inputDia.value == '' || inputDia.value == null || inputDia.value == undefined) {
+        imprimirMsjError('El campo Dia es requerido');
+    }
+    if (inputMes.value == '' || inputMes.value == null || inputMes.value == undefined) {
+        imprimirMsjError('El campo Mes es requerido');
+    }
+    if (inputAnio.value == '' || inputAnio.value == null || inputAnio.value == undefined) {
+        imprimirMsjError('El campo Año es requerido');
+    }
+    if (fotoPerfil.value == '' || fotoPerfil.value == null || fotoPerfil.value == undefined) {
+        imprimirMsjError('Por favor ingrese una foto de perfil!');
+    }
+});
 
 
 // submitRegistrate.addEventListener('click', function(){
@@ -98,4 +132,18 @@ function imprimirMsjCorrecto(pmensaje) {
         icon: 'success',
         confirmButtonText: 'OK'
     });
+}
+
+function resaltarLabelIncorrecto(pLabelID) {
+    let elementLabel = document.getElementById(pLabelID);
+    let sytleOrigin = elementLabel.style;
+
+    elementLabel.style = 'color:red;';
+}
+
+function resaltarInputIncorrecto(pInputID) {
+    let elementInput = document.getElementById(pInputID);
+    let sytleOrigin = elementInput.style;
+
+    elementInput.style = 'border: 1px solid red;';
 }
